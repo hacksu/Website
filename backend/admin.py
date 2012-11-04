0d0b654c3d76a319926d8dcf79d9458bb93d94ad
danielgur <danielgur2105@gmail.com>
2012-11-03 20:00:43 -0400

    Adding the models

---
+++ backend/admin.py
@@ -0,0 +1,7 @@
+from backend.models import News, Link, Leader, Apps
+from django.contrib import admin
+
+admin.site.register(News)
+admin.site.register(Link)
+admin.site.register(Leader)
+admin.site.register(Apps)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    url = models.CharField(max_length=500)
+    title = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.title
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=3000)
+    links = models.ManyToManyField(Link)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+
+class Leader(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=200, blank=True)
+    description = models.CharField(max_length=1500)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Apps(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=3000)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Apps'
+        verbose_name_plural = 'Apps'
+

d7792f78a7bfdb39117d456b18d480573c90d171
danielgur <danielgur2105@gmail.com>
2012-11-03 20:45:31 -0400

    Cleaning url file

--- hacksu/urls.py
+++ hacksu/urls.py
@@ -9,8 +9,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'hacksu.views.home', name='home'),
     # url(r'^hacksu/', include('hacksu.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )

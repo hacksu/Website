11e700c27c24154d3e53eab2b03ddba47e9ddcfa
Camden Fullmer <camdenfullmer@gmail.com>
2012-11-06 23:54:37 -0500

    Added all views for different pages.

--- backend/views.py
+++ backend/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def leaders(request):
     return render_to_response('leaders.html', RequestContext(request))
+
+def leaders(request):
+    return render_to_response('apps.html', RequestContext(request))
+
+def leaders(request):
+    return render_to_response('news.html', RequestContext(request))
+
+def leaders(request):
+    return render_to_response('learn.html', RequestContext(request))
+
+def leaders(request):
+    return render_to_response('contact.html', RequestContext(request))

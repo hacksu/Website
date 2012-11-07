fb7c6bcfc530bec0d14187c7f2ee5fc4eca8bb55
Camden Fullmer <camdenfullmer@gmail.com>
2012-11-06 23:56:07 -0500

    Added all urls for different pages.

--- hacksu/urls.py
+++ hacksu/urls.py
@@ -7,6 +7,10 @@ admin.autodiscover()
 urlpatterns = patterns('backend.views',
     url(r'^$', 'index'),
     url(r'^leaders$', 'leaders'),
+    url(r'^apps$', 'apps'),
+    url(r'^contact$', 'contact'),
+    url(r'^learn$', 'learn'),
+    url(r'^news$', 'news'),
 )
 
 urlpatterns += patterns('',

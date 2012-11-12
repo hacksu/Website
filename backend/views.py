d4e479ce06c59283ca49658280a618ea19cce830
danielgur <danielgur2105@gmail.com>
2012-11-11 23:02:19 -0500

    Adding single template files for apps and news

--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render_to_response,  get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from backend.models import News, NewsLink, Leader, Apps, AppsLink, NewsTopic
 
 def index(request):
     apps = Apps.objects.all()
     news = News.objects.all().order_by('-date')
-    return render_to_response('index.html',{'apps': apps, 'news_list': news},
+    return render_to_response('index.html', {'apps': apps, 'news_list': news},
                               RequestContext(request))
 
 def leaders(request):
@@ -13,16 +13,26 @@ def leaders(request):
     return render_to_response('leaders.html', {'leaders': leaders},
                               RequestContext(request))
 
-def apps(request):
-    apps = Apps.objects.all()
-    return render_to_response('apps.html', {'apps': apps},
-                              RequestContext(request))
-
-def news(request):
-    news = News.objects.all().order_by('-date')
-    return render_to_response('news.html', {'news_list': news},
-                              RequestContext(request))
-
+def apps(request, app_id=None):
+    if not app_id:
+        apps = Apps.objects.all()
+        return render_to_response('apps.html', {'apps': apps},
+                                  RequestContext(request))
+    else:
+        app = Apps.objects.get(id=app_id)
+        return render_to_response('apps_single.html', {'app': app},
+                                  RequestContext(request))
+        
+def news(request, news_id=None):
+    if not news_id:
+        news = News.objects.all().order_by('-date')
+        return render_to_response('news.html', {'news_list': news},
+                                  RequestContext(request))
+    else:
+        news = News.objects.get(id=news_id)
+        return render_to_response('news_single.html', {'news': news},
+                                  RequestContext(request))
+        
 def learn(request):
     return render_to_response('learn.html', RequestContext(request))
 
--- hacksu/urls.py
+++ hacksu/urls.py
@@ -7,9 +7,12 @@ urlpatterns = patterns('backend.views',
     url(r'^$', 'index'),
     url(r'^leaders$', 'leaders'),
     url(r'^apps$', 'apps'),
+    url(r'^apps/(?P<app_id>\d+)/$', 'apps'),
     url(r'^contact$', 'contact'),
     url(r'^learn$', 'learn'),
     url(r'^news$', 'news'),
+    url(r'^news/(?P<news_id>\d+)/$', 'news'),
+
 )
 
 urlpatterns += patterns('',

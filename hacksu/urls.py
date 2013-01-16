4167a8a96a14255b4a40099c19857917c2b9543d
danielgur <danielgur2105@gmail.com>
2013-01-15 22:28:28 -0500

    Added links for news and apps in index.html for #15 and fixed our url regex's

--- hacksu/urls.py
+++ hacksu/urls.py
@@ -1,19 +1,17 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('backend.views',
     url(r'^$', 'index'),
-    url(r'^leaders$', 'leaders'),
-    url(r'^apps$', 'apps'),
+    url(r'^leaders/$', 'leaders'),
+    url(r'^apps/$', 'apps'),
     url(r'^apps/(?P<app_id>\d+)/$', 'apps'),
-    url(r'^contact$', 'contact'),
-    url(r'^learn$', 'learn'),
-    url(r'^news$', 'news'),
+    url(r'^contact/$', 'contact'),
+    url(r'^learn/$', 'learn'),
+    url(r'^news/$', 'news'),
     url(r'^news/(?P<news_id>\d+)/$', 'news'),
-    url(r'^hackathon$', 'hackathon'),
-    url(r'^pull$', 'pull'),
+    url(r'^hackathon/$', 'hackathon'),
     url(r'^leaders/(\d{1,2})/$', 'leader'),
 )
 

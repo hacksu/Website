488206bf7bed6d24a5b73aba8e53b9e0ec502414
Camden Fullmer <camdenfullmer@gmail.com>
2013-02-28 03:13:30 -0500

    Added files.

---
+++ hacksu/admin.py
@@ -0,0 +1,9 @@
+from hacksu.models import News, NewsLink, Leader, Apps, AppsLink, NewsTopic
+from django.contrib import admin
+
+admin.site.register(News)
+admin.site.register(NewsLink)
+admin.site.register(Leader)
+admin.site.register(Apps)
+admin.site.register(AppsLink)
+admin.site.register(NewsTopic)

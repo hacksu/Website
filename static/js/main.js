2e14d61db2a6b9e35d35755433a8d738d0af9160
Camden Fullmer <camdenfullmer@gmail.com>
2013-09-28 02:38:23 -0400

    First push of the real design.

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,5 @@
-// Change the widths of the content based on the width of the window.
-
-$(window).resize(function() {
-  if($('#content_main').width() < 900){
-	$('#news').width('100%');
-	$('#apps').width('100%');
-	$('#feed').width('100%');	  
-  }else{
-	$('#news').width('33.33%');
-	$('#apps').width('33.33%');
-	$('#feed').width('33.33%'); 
-  }
-});
+function showAbout() {
+    $('html, body').animate({
+        scrollTop: $("#about-info").offset().top - 50
+    }, 500);
+}

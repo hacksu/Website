bf48af5fb9b8ec7073c1adb868dd8070dd25171c
Camden Fullmer <camdenfullmer@gmail.com>
2013-10-07 23:34:09 -0400

    Added awesome javascript.

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,35 @@ function showAbout() {
         scrollTop: $("#about-info").offset().top - 50
     }, 500);
 }
+
+function showWhat() {
+    $('html, body').animate({
+        scrollTop: $("#what").offset().top
+    }, 500);
+}
+
+function showWhen() {
+    $('html, body').animate({
+        scrollTop: $("#when").offset().top
+    }, 500);
+}
+
+function showWhere() {
+    $('html, body').animate({
+        scrollTop: $("#where").offset().top
+    }, 500);
+}
+
+function showRegistration() {
+    $('html, body').animate({
+        scrollTop: $("#registration").offset().top
+    }, 500);
+}
+
+function showSponsors() {
+    $('html, body').animate({
+        scrollTop: $("#sponsors").offset().top
+    }, 500);
+}
+
+

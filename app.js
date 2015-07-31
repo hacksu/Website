d1768028da24471cfd697edefc2a9301e0031426
Robin Bonatesta <rbonate@us.ibm.com>
2015-07-31 16:49:34 -0500

    Add favicon and change pictures

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use('/favicon.ico', express.static('public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());

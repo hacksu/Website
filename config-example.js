25655282a607927c0319795028d68494b85fb300
Isaac Park <isaac.a.park@gmail.com>
2016-09-25 17:58:36 -0400

    Update Example Config

--- config-example.js
+++ config-example.js
@@ -1,6 +1,11 @@
-
 exports.mailingList = {
         url: "https://listmail.cs.kent.edu/mailman/admin/",
         list: "hacksu-list",
-        password: "..."
+        password: ""
 }
+
+exports.mongodbUrl = "mongodb://localhost:27017/hacksu"
+
+exports.users = [
+        "110947483314315320479"
+]

aed2e43c008c6e476b14ca03aa1daae5350bde3a
Isaac Park <isaac.a.park@gmail.com>
2016-09-25 19:07:16 -0400

    Cleanup API

--- routes/api.js
+++ routes/api.js
@@ -82,7 +82,7 @@ MongoClient.connect(config.mongodbUrl, function(err, db) {
 
     router.get("/events", get_events);
 
-    router.use("/event", authentication);
+    router.use("/event", authentication);  // check requests for login information and set req.authenticated to true if the user has access
     router.post("/event", function(req, res){
         if (!req.authenticated) {
             res.status(403).json({error: "Not authorized"});
@@ -91,9 +91,8 @@ MongoClient.connect(config.mongodbUrl, function(err, db) {
             if (err === null) {
                 get_events(req, res);
             } else {
-                get_events(req, res);
+                get_events(req, res); // right now down't provide an error when something goes wrong
             }
-            console.log("tried to add",err)
         });
     });
 
@@ -107,7 +106,6 @@ MongoClient.connect(config.mongodbUrl, function(err, db) {
             } else {
                 get_events(req, res);
             }
-            console.log("tried to remove",err)
         });
     });
 
@@ -121,86 +119,10 @@ MongoClient.connect(config.mongodbUrl, function(err, db) {
             } else {
                 get_events(req, res);
             }
-            console.log("tried to modify",err)
         });
     });
 });
 
-
-exports.events = function(req, res){
-  res.json({
-      events: [
-          {
-              "date": Date.parse("2016-08-30 19:00"),
-              "title": "Meet and Greet",
-              "id": 1,
-              "content": `
-              <p>Hey Hackers!</p>
-              <p>
-                  This will be our first meeting of the semester. Welcome back everyone.
-                  We'll mostly just be hanging out and getting to know folks, but I'd
-                  also love to talk about what's new this year. Things people want
-                  to build and some resources to help people learn to program.
-              </p>
-              `
-          },
-          {
-              "date": Date.parse("2016-09-06 19:00"),
-              "title": "Websites: Doable",
-              "id": 2,
-              "content": `
-              <p>Hey Hackers!</p>
-              <p>
-                This week we'll be talking about websites. We'll be giving two talks.
-                Ben will talk about HTML the basic building block of all sites
-                and Isaac will talk about React a popular modern framework for web development
-              </p>
-              `
-          },
-          {
-              "date": Date.parse("2016-09-13 19:00"),
-              "title": "Scripting",
-              "id": 3,
-              "content": `
-              <p>Hey Hackers!</p>
-              <p>
-                Again this week we will be giving two talks. Ben will give a talk
-                on the basics of javascript and programing more generally and Isaac
-                will talk about Rust a modern systems programing language.
-              </p>
-              `
-          },
-          {
-              "date": Date.parse("2016-09-20 19:00"),
-              "title": "Git",
-              "id": 4,
-              "content": `
-              <p>Hey Hackers!</p>
-              <p>
-                If you want to write code, you'll want to learn Git. 
-                Git keeps track of all of your changes and makes collaboration with others easy!
-                Nick will be showing you basic git commands to get up and running as well as best
-                practices when working in teams.
-              </p>
-              `
-          },
-          {
-              "date": Date.parse("2016-09-27 19:00"),
-              "title": "Mini Hackathon",
-              "id": 5,
-              "content": `
-              <p>Hey Hackers!</p>
-              <p>
-                We all had a lot of fun last semester with a mini-hackathon,
-                so we're doing it again. Here's what going to happen. Everyone will
-                break up into groups and then your have about an hour to make something
-                based on a couple random words we'll give as a prompt.
-              </p>
-              `
-         }
-      ]
-  });
-};
 
 exports.addToMailingList = function(req, res) {
     mailingList.add(req.body.email, function (success) {
